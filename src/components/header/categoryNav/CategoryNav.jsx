69bca9fc5213fef186b6c9e117314fed641dc8d8
minsik2434 <minsik2434@gmail.com>
2025-04-16 15:45:38 +0900

    Feat: 헤더 구현

--- src/components/header/categoryNav/CategoryNav.jsx
+++ src/components/header/categoryNav/CategoryNav.jsx
@@ -1,14 +1,23 @@
 import styles from "./CategoryNav.module.css";
 import hamburgerIcon from "../../../assets/images/hamburger.svg";
+import CategoryNavDropBox from "./CategoryNavDropBox";
+import { useState } from "react";
 const CategoryNav = () => {
+  const [dropState, setDropState] = useState(false);
   return (
     <div className={styles.wrapper}>
-      <button className={styles.categoryButton}>
+      <button
+        className={styles.categoryButton}
+        onClick={() => {
+          setDropState((prev) => !prev);
+        }}
+      >
         <div className={styles.buttonWrapper}>
           <img src={hamburgerIcon} style={{ width: "25px" }} />
           <span className={styles.buttonText}>카테고리</span>
         </div>
       </button>
+      {dropState && <CategoryNavDropBox />}
     </div>
   );
 };
---
+++ src/components/header/categoryNav/CategoryNavDropBox.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./CategoryNav.module.css";
+const CategoryNavDropBox = () => {
+  return (
+    //TODO 카테고리 정보 조회해서 리스트 생성하도록해야함 현재는 그냥 코드로 적었음
+    <div className={styles.categoryWrapper}>
+      <ul>
+        <li>
+          <Link className={styles.categoryItem}>식품</Link>
+        </li>
+        <li>
+          <Link className={styles.categoryItem}>전자기기</Link>
+        </li>
+        <li>
+          <Link className={styles.categoryItem}>의류</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default CategoryNavDropBox;
