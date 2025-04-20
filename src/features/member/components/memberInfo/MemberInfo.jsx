21fcdadac9495057884bd849c1b560e5b932ad70
minsik2434 <minsik2434@gmail.com>
2025-04-21 01:24:31 +0900

    Feat: mypage 회원 정보 컴포넌트 추가

---
+++ src/features/member/components/memberInfo/MemberInfo.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import styles from "./MemberInfo.module.css";
+import orderIcon from "../../../../assets/images/order.svg";
+import cartIcon from "../../../../assets/images/cart.svg";
+import coinIcon from "../../../../assets/images/coin.svg";
+import couponIcon from "../../../../assets/images/coupon.svg";
+const MemberInfo = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.introductionWrapper}>
+        <div className={styles.nameWrapper}>
+          <span className={styles.memberName}>최민식</span>
+          <span>님 환영합니다</span>
+        </div>
+        <div className={styles.remainBalanceWrapper}>
+          <span>잔액</span>
+          <span className={styles.remainBalance}>20000원</span>
+        </div>
+      </div>
+      <div className={styles.myMenuWrapper}>
+        <Link className={styles.link}>
+          <div className={styles.myMenuIconWrapper}>
+            <img className={styles.menuIcon} src={orderIcon} />
+          </div>
+          <div className={styles.myMenu}>
+            <span className={styles.menuName}>주문</span>
+            <span className={styles.menuValue}>10건 </span>
+          </div>
+        </Link>
+        <Link className={styles.link}>
+          <div className={styles.myMenuIconWrapper}>
+            <img className={styles.menuIcon} src={cartIcon} />
+          </div>
+          <div className={styles.myMenu}>
+            <span className={styles.menuName}>장바구니</span>
+            <span className={styles.menuValue}>10000원</span>
+          </div>
+        </Link>
+        <Link className={styles.link}>
+          <div className={styles.myMenuIconWrapper}>
+            <img className={styles.menuIcon} src={coinIcon} />
+          </div>
+          <div className={styles.myMenu}>
+            <span className={styles.menuName}>적립금</span>
+            <span className={styles.menuValue}>10000원</span>
+          </div>
+        </Link>
+        <Link className={styles.link}>
+          <div className={styles.myMenuIconWrapper}>
+            <img className={styles.menuIcon} src={couponIcon} />
+          </div>
+          <div className={styles.myMenu}>
+            <span className={styles.menuName}>쿠폰</span>
+            <span className={styles.menuValue}>5장</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MemberInfo;
--- src/pages/mypage/order/OrderListPage.jsx
+++ src/pages/mypage/order/OrderListPage.jsx
@@ -1,7 +1,14 @@
+import MemberInfo from "../../../features/member/components/memberInfo/MemberInfo";
 import MyPageLayout from "../../../layout/MyPageLayout";
-
+import styles from "./OrderList.module.css";
 const OrderList = () => {
-  return <MyPageLayout>asdf</MyPageLayout>;
+  return (
+    <MyPageLayout>
+      <div className={styles.container}>
+        <MemberInfo />
+      </div>
+    </MyPageLayout>
+  );
 };
 
 export default OrderList;
