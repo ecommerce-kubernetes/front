847db68a8bc27170474ac07427549e4025f36b57
minsik2434 <minsik2434@gmail.com>
2026-03-10 20:19:36 +0900

    Feat: 상단 헤더 구현 헤더 상단 부분인 검색창, 유틸리티 바 구현 Resolves: #2

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,67 @@
+"use client";
+import { useState } from "react";
+import Logo from "../common/Logo";
+import { Search, X, User, ShoppingCart } from "lucide-react";
+import Link from "next/link";
+
 export default function Header() {
+  const [keyword, setKeyword] = useState("");
+  const handleClear = () => {
+    setKeyword("");
+  };
   return (
     <>
       <header className="w-full bg-white">
-        <div className="w-full max-w-[1200px] mx-auto">
+        <div className="w-full max-w-250 mx-auto">
           <div className="flex justify-end h-8">
-            <ul className="flex gap-4 text-xs text-black h-full">
-              <li className="h-full flex items-center cursor-pointer">
+            <ul className="flex text-xs text-black h-full">
+              <li className="after:content-['|'] after:mx-2 after:text-gray-300 last:after:content-none h-full flex items-center cursor-pointer">
                 회원가입
               </li>
-              <li className="h-full flex items-center cursor-pointer">
+              <li className="after:content-['|'] after:mx-2 after:text-gray-300 last:after:content-none h-full flex items-center cursor-pointer">
                 로그인
               </li>
-              <li className="h-full flex items-center cursor-pointer">
+              <li className="after:content-['|'] after:mx-2 after:text-gray-300 last:after:content-none h-full flex items-center cursor-pointer">
                 고객센터
               </li>
             </ul>
           </div>
-          <div className="h-20 flex items-center">
-            로고, 장바구니, 마이페이지, 검색바 영역
+          <div className="flex justify-between items-center mt-5">
+            <Logo className="w-40 text-brand-primary flex-shrink-0" />
+            <form className="border border-brand-primary rounded-full h-12 w-130 flex justify-between items-center">
+              <div className="relative flex-1 h-full flex items-center">
+                <input
+                  type="text"
+                  placeholder="상품을 검색하세요"
+                  value={keyword}
+                  onChange={(e) => setKeyword(e.target.value)}
+                  className="outline-none flex-1 h-full bg-transparent px-5 pr-10"
+                />
+                {keyword && (
+                  <button
+                    type="button"
+                    onClick={handleClear}
+                    className="absolute cursor-pointer right-2 text-gray-400 hover:text-brand-primary transition-colors"
+                  >
+                    <X size={20} />
+                  </button>
+                )}
+              </div>
+              <button
+                type="submit"
+                className="px-4 h-full cursor-pointer rounded-r-full bg-brand-primary font-bold"
+              >
+                <Search className="text-white" size={24} strokeWidth={2} />
+              </button>
+            </form>
+            <div className="flex gap-5">
+              <Link href="/">
+                <User size={30} />
+              </Link>
+              <Link href="/">
+                <ShoppingCart size={30} />
+              </Link>
+            </div>
           </div>
         </div>
       </header>
