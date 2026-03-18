eeb57b45c4553693f59b30dcf91a9f7b0433d5d4
minsik2434 <minsik2434@gmail.com>
2026-03-18 18:57:59 +0900

    Feat: 입력창 마크업
    Resolves: #5

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,9 +5,22 @@ export default function LoginPage() {
   return (
     <section className="w-full max-w-250 mx-auto flex items-center my-20 flex-col">
       <h1 className="font-bold text-xl font-pretendard">로그인</h1>
-      <form className="mt-10 w-70">
-        <InputBox headImg={Mail} keyword="" />
-        <InputBox headImg={Lock} keyword="" />
+      <form className="mt-5 w-80 flex flex-col gap-2">
+        <InputBox
+          headImg={Mail}
+          value=""
+          placeHolder="이메일을 입력해주세요"
+          type="text"
+        />
+        <InputBox
+          headImg={Lock}
+          value=""
+          placeHolder="비밀번호를 입력해주세요"
+          type="password"
+        />
+        <button className="w-full py-3 cursor-pointer text-white bg-brand-primary rounded-sm">
+          로그인
+        </button>
       </form>
     </section>
   );
--- src/components/common/InputBox.tsx
+++ src/components/common/InputBox.tsx
@@ -2,15 +2,26 @@ import { LucideIcon } from "lucide-react";
 
 interface InputBoxProps {
   headImg: LucideIcon;
-  keyword: string;
+  value: string;
+  placeHolder: string;
+  type: string;
 }
 
-export default function InputBox({ headImg, keyword }: InputBoxProps) {
+export default function InputBox({
+  headImg,
+  value,
+  placeHolder,
+  type,
+}: InputBoxProps) {
   const IconComponent = headImg;
   return (
-    <div className="w-full flex border">
+    <div className="w-full border border-gray-300 flex rounded-sm px-2 py-3 focus-within:border-brand-primary bg-white">
       <IconComponent size={26} />
-      <input />
+      <input
+        className="flex-1 pl-2 outline-none text-sm"
+        type={type}
+        placeholder={placeHolder}
+      />
     </div>
   );
 }
