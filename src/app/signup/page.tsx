25b443059fd9093893d1a9d92c3aae12116e4b32
minsik2434 <minsik2434@gmail.com>
2026-03-20 19:29:50 +0900

    Feat: 회원가입 페이지 마크업
    Resolves: #8

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,36 @@
+import InputBox from "@/src/components/common/InputBox";
+import { Lock, LockKeyhole, Mail, Phone, User } from "lucide-react";
+
+export default function SignupPage() {
+  return (
+    <section className="w-full max-w-250 mx-auto flex items-center my-20 flex-col">
+      <h1 className="font-bold text-xl font-pretendard">회원 가입</h1>
+      <form className="mt-5 w-80 flex flex-col gap-2">
+        <InputBox headImg={Mail} type="text" placeholder="이메일" />
+        <InputBox headImg={Lock} type="password" placeholder="비밀번호" />
+        <InputBox
+          headImg={LockKeyhole}
+          type="password"
+          placeholder="비밀번호 확인"
+        />
+        <div>
+          <input type="radio" name="gender" value="MALE" />
+          <input type="radio" name="gender" value="FEMALE" />
+        </div>
+        <div>
+          <select>
+            <option>asdfasd</option>
+          </select>
+          <select>
+            <option>asdfasd</option>
+          </select>
+          <select>
+            <option>asdfasd</option>
+          </select>
+        </div>
+        <InputBox headImg={User} type="text" placeholder="이름" />
+        <InputBox headImg={Phone} type="text" placeholder="전화번호" />
+      </form>
+    </section>
+  );
+}
--- src/providers/MSWComponent.tsx
+++ src/providers/MSWComponent.tsx
@@ -1,8 +1,19 @@
 "use client";
 import { useEffect, useState } from "react";
 
+declare global {
+  interface Window {
+    __MSW_STARTED__?: boolean;
+  }
+}
+
 export const MSWComponent = ({ children }: { children: React.ReactNode }) => {
-  const [mswReady, setMswReady] = useState(false);
+  const [mswReady, setMswReady] = useState(() => {
+    if (typeof window !== "undefined") {
+      return window.__MSW_STARTED__ || false;
+    }
+    return false;
+  });
 
   useEffect(() => {
     const init = async () => {
@@ -10,8 +21,13 @@ export const MSWComponent = ({ children }: { children: React.ReactNode }) => {
         process.env.NODE_ENV === "development" &&
         process.env.NEXT_PUBLIC_API_MOCKING === "enabled"
       ) {
+        if (window.__MSW_STARTED__) {
+          setMswReady(true);
+          return;
+        }
         const { worker } = await import("@/src/mocks/browser");
         await worker.start({ onUnhandledRequest: "bypass" });
+        window.__MSW_STARTED__ = true;
       }
       setMswReady(true);
     };
