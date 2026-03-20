187ff4c6cbf79f6223b98e12febbb487a176bc12
최민식 <119111149+minsik2434@users.noreply.github.com>
2026-03-21 02:15:13 +0900

    Refactor/mocking (#12)
    * Fix: mocks 폴더 삭제
    
    Resolves: #11
    
    * Fix: instrumentation.ts 삭제
    
    Resolves: #11
    
    * Fix: MSW 프로바이더 삭제
    
    Resolves: #11
    
    * Fix: msw 의존성 제거
    
    Resolves: #11
    
    * Feat: json api 모킹 서버 구축
    
    Resolves: #11
    
    * Test: api 호출 테스트
    
    Resolves: #11

---
+++ mocks/server.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+
+const productRouter = require("./routes/product-service");
+const userRouter = require("./routes/user-service");
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.use("/product-service", productRouter);
+server.use("/user-service", userRouter);
+
+server.listen(8000, () => {
+  console.log("mock API 게이트웨이 실행");
+})
