75e8caaa6c02ac69edef993e72aa4961064cc93f
minsik2434 <minsik2434@gmail.com>
2026-04-02 09:31:40 +0900

    Feat: 상품 리뷰 이미지 뷰어 구현
    Resolves: #16

--- src/components/reviews/ReviewList.tsx
+++ src/components/reviews/ReviewList.tsx
@@ -1,5 +1,7 @@
 import { useModal } from "@/src/hooks/queries/useModal";
-import { Star, X } from "lucide-react";
+import { Star } from "lucide-react";
+import { ReviewImageView } from "../modal/ReviewImageView";
+import { useState } from "react";
 
 export const ReviewList = () => {
   const mockImage = [
@@ -8,12 +10,19 @@ export const ReviewList = () => {
     "https://img-cf.kurly.com/hdims/resize/%5E%3E240x%3E240/cropcenter/240x240/quality/85/src/shop/data/review/20251102/116f50c9-b3d3-422d-9157-65fc62bc694e.jpg",
     "https://img-cf.kurly.com/hdims/resize/%5E%3E240x%3E240/cropcenter/240x240/quality/85/src/shop/data/review/20251102/116f50c9-b3d3-422d-9157-65fc62bc694e.jpg",
     "https://img-cf.kurly.com/hdims/resize/%5E%3E240x%3E240/cropcenter/240x240/quality/85/src/shop/data/review/20251102/116f50c9-b3d3-422d-9157-65fc62bc694e.jpg",
+    "https://istore.xcache.kinxcdn.com/prd/data/goods/1/2025/02/135_temp_17403760727119large.jpg",
   ];
 
   const MAX_DISPLAY = 4;
   const hasMoreImage = mockImage.length > MAX_DISPLAY;
   const extraCount = mockImage.length - MAX_DISPLAY;
   const { isModalOpen, modalOpen, modalClose } = useModal();
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
+  const handleImageClick = (index: number) => {
+    setSelectedIndex(index);
+    modalOpen();
+  };
 
   return (
     <>
@@ -47,7 +56,7 @@ export const ReviewList = () => {
                 <li
                   key={index}
                   className="relative shrink-0 w-25 h-25 rounded-md overflow-hidden cursor-pointer"
-                  onClick={modalOpen}
+                  onClick={() => handleImageClick(index)}
                 >
                   <img className="w-full h-full object-cover" src={imgUrl} />
                   {isLastVisibleImage && hasMoreImage && (
@@ -62,40 +71,11 @@ export const ReviewList = () => {
         </div>
       </div>
       {isModalOpen && (
-        <div className="fixed inset-0 z-50 bg-black/60 flex flex-col">
-          <div className="flex-1 flex items-center justify-center">
-            <div className="w-200 h-170 bg-white rounded-2xl flex items-center justify-center relative">
-              <button
-                className="absolute top-3 right-3 cursor-pointer"
-                onClick={modalClose}
-              >
-                <X width={32} height={32} />
-              </button>
-              <div className="flex flex-col gap-10 items-center">
-                <div className="relative shrink-0 w-100 h-120 rounded-md overflow-hidden">
-                  <img
-                    className="w-full h-full object-cover"
-                    src={
-                      "https://img-cf.kurly.com/hdims/resize/%5E%3E240x%3E240/cropcenter/240x240/quality/85/src/shop/data/review/20251102/116f50c9-b3d3-422d-9157-65fc62bc694e.jpg"
-                    }
-                  />
-                </div>
-                <div>
-                  <ul className="flex gap-2">
-                    {mockImage.map((image, index) => (
-                      <li key={index} className="w-12 h-12 cursor-pointer">
-                        <img
-                          className="w-full h-full object-cover"
-                          src={image}
-                        />
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ReviewImageView
+          initialIndex={selectedIndex}
+          mockImage={mockImage}
+          close={modalClose}
+        />
       )}
     </>
   );
